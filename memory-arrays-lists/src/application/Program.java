0b15c5b4132c752901f277f86544bc2eee1fe1c3
Loyane Menezes <loyanesezenem5@gmail.com>
2025-07-23 21:05:02 -0300

    feat: adiciona primeiro exercício sobre memória, arrays e listas

---
+++ memory-arrays-lists/src/application/Program.java
@@ -0,0 +1,29 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("How many numbers are you going to enter: ");
+        int n = sc.nextInt();
+        int[] vect = new int[n];
+
+        for (int i = 0; i < vect.length; i++) {
+            System.out.print("Enter a number: ");
+            vect[i] = sc.nextInt();
+        }
+
+        System.out.println("NEGATIVE NUMBERS:");
+        for (int i = 0; i < vect.length; i++) {
+            if (vect[i] < 0) {
+                System.out.println(vect[i]);
+            }
+        }
+
+        sc.close();
+    }
+}
